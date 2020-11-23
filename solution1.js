5a406aa3543a9a7f4c7862fd82de4dbab7bd9976
onkar915 <62669765+onkar915@users.noreply.github.com>
2020-11-23 16:10:26 +0100

    Create solution1.js

---
+++ solution1.js
@@ -0,0 +1,5 @@
+function lifePathNumber(dateOfBirth) {
+  return [...dateOfBirth.split('-')].map(v=>v.split('').reduce((a,b)=>a+b*1,0))
+ .map(v=>v.toString().split('').reduce((a,b)=>a+b*1,0)).reduce((a,b)=>a+b*1,0).toString().split('').reduce((a,b)=>a+b*1,0)
+ .toString().split('').reduce((a,b)=>a+b*1,0)
+}
