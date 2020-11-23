e34863b8ce1202940e987360136d776dd7c60520
onkar915 <62669765+onkar915@users.noreply.github.com>
2020-11-23 16:18:21 +0100

    Update Challenge-solution4.js

--- Challenge-solution4.js
+++ Challenge-solution4.js
@@ -1,1 +1,20 @@
 
+function petalFall(v) {
+
+  let distToGround = 5 * 80; 
+  let time = 0;
+  
+  if(v > 0){
+    time = distToGround / v;
+  }
+  
+  return time;
+
+}
+
+function petalFall(v) {
+  return v < 1 ? 0 : 400 / v;
+}
+
+
+const petalFall = v => v>0 ? 400 / v : 0 ;
