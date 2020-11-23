19de63a0cbf649210855b488662b6b260b340ebd
onkar915 <62669765+onkar915@users.noreply.github.com>
2020-11-23 16:22:29 +0100

    Update Challenge-solution3.js

--- Challenge-solution3.js
+++ Challenge-solution3.js
@@ -1,1 +1,7 @@
 
+let name='ONKArrrr'
+let big=name.split('').filter(n => n===n.toUpperCase()).length;
+console.log(big);
+let small=name.length-big;
+console.log(small);
+console.log((big>small)?name.toUpperCase():name.toLowerCase());
